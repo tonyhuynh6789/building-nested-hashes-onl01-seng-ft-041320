60868aef2c524d8a9dc0a6f11ee6d0abdd42ffb7
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-24 23:18:00 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,12 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {
-        :name => "Lord Montague"
-        :age => "53"
+         name: "Lord Montague",
+         age: => "53"
       },
-      :matriarch => {},
+      :matriarch => {
+        :name => 
+      },
       :hero => {},
       :hero_friends => []
       }, 
